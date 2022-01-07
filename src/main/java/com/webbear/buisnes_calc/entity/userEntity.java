33686734e703f3c8645d8aec16a32e751c04dced
WEBmen <shalcer13@mail.ru>
2022-01-07 17:22:46 +0300

    all worked

--- src/main/java/com/webbear/buisnes_calc/entity/userEntity.java
+++ src/main/java/com/webbear/buisnes_calc/entity/userEntity.java
@@ -11,30 +11,30 @@ public class userEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String userName;
+    private String username;
     private String password;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "userEntity")
-    private List<calculatedTaxation> taxations;
+//    @OneToMany(cascade = CascadeType.ALL, mappedBy = "userEntity")
+//    private List<calculatedTaxation> taxations;
 
     public userEntity(){
 
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
